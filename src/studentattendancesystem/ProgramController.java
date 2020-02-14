bb4317ef701ecb1a36549c666238aed6a4d9f6e2
SerireS <55481182+SerireS@users.noreply.github.com>
2020-02-14 08:27:51 +0100

    Update gui

--- src/studentattendancesystem/ProgramController.java
+++ src/studentattendancesystem/ProgramController.java
@@ -9,6 +9,9 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.image.ImageView;
 
 /**
@@ -27,6 +30,5 @@ public class ProgramController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }     
 }
