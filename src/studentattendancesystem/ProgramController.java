da47324dc40c24acbff5b00cd15414ebf3f32b95
SerireS <55481182+SerireS@users.noreply.github.com>
2020-02-20 18:42:14 +0100

    MockData

--- src/studentattendancesystem/ProgramController.java
+++ src/studentattendancesystem/ProgramController.java
@@ -5,7 +5,9 @@
  */
 package studentattendancesystem;
 
+import com.jfoenix.controls.JFXListView;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,6 +18,8 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.image.ImageView;
+import studentattendancesystem.be.Student;
+import studentattendancesystem.dal.StudentDAO;
 
 /**
  * FXML Controller class
@@ -27,32 +31,28 @@ public class ProgramController implements Initializable {
     @FXML
     private ImageView Image;
     @FXML
-    private PieChart pieChart;
-    @FXML
     private LineChart<Number, Number> lineChart;
+    @FXML
+    private JFXListView<String> txtDays;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
+        StudentDAO studentDao = new StudentDAO();
+        List<Student> studentMock = studentDao.studentMock();
+        
+        txtDays.getItems().addAll(studentMock.get(1).getdays());
+        
         XYChart.Series dataSeries1 = new XYChart.Series();
         dataSeries1.setName("2014");
 
         dataSeries1.getData().add(new XYChart.Data("Mandag", 0));
         dataSeries1.getData().add(new XYChart.Data("Tirsdag", 0));
-        dataSeries1.getData().add(new XYChart.Data("Onsdag", 15));
+        dataSeries1.getData().add(new XYChart.Data("Onsdag", 33));
 
         lineChart.getData().add(dataSeries1);
-
-        PieChart pieChart = new PieChart();
-
-        PieChart.Data slice1 = new PieChart.Data("absence", 15);
-        PieChart.Data slice2 = new PieChart.Data("present", 75);
-
-
-        pieChart.getData().add(slice1);
-        pieChart.getData().add(slice2);
-
     }
 }
