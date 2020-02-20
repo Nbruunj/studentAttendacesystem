291d7e6335caa5bd809861759e30ffbdf49793a0
SerireS <55481182+SerireS@users.noreply.github.com>
2020-02-20 16:26:56 +0100

    Update

--- src/studentattendancesystem/be/Student.java
+++ src/studentattendancesystem/be/Student.java
@@ -12,11 +12,35 @@ package studentattendancesystem.be;
 public class Student
 {
     private String name;
+    private int id;
+    private String grade;
+    
+    public Student(int i, String bilbo_Ba, String sco) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
-    public void student (String name)
+    public void student (int id, String name, String grade)
     {
         this.name = name;
+        this.id = id;
+        this.grade = grade;
+    }
+    
+    /*
+     * Getter for Id
+     */
+    public int getId()
+    {
+        return id;
+    }
+
+    /*
+     * Setter for Id
+     */
+    public void setId(int id)
+    {
+        this.id = id;
     }
     
     /*
@@ -34,4 +58,21 @@ public class Student
     {
         this.name = name;
     }
+    
+        /*
+     * Getter for grade
+     */
+    public String getGrade()
+    {
+        return grade;
+    }
+
+    /*
+     * Setter for grade
+     */
+    public void setId(String grade)
+    {
+        this.grade = grade;
+    }
+    
 }
